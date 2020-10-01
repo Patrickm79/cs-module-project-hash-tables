e69d29594c81fe37b485c858089d3f3c0de01479
Patrick Millet <lohs79seahawk@gmail.com>
2020-10-01 16:30:15 -0500

    recursive solution to expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -3,9 +3,19 @@
 
 def expensive_seq(x, y, z):
     # Your code here
+    cache = {}
 
+    def nest_expseq(x, y, z):
+        if x <= 0:
+            return y + z
+        
+        if (x, y, z) not in cache:
+            cache[(x, y, z)] = nest_expseq(x-1,y+1,z) + nest_expseq(x-2,y+2,z*2) + nest_expseq(x-3,y+3,z*3)
 
-
+        return cache[(x, y, z)]
+        
+    return nest_expseq(x, y, z)
+    
 if __name__ == "__main__":
     for i in range(10):
         x = expensive_seq(i*2, i*3, i*4)
